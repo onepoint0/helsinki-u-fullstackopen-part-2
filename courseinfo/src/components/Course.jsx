776429429d6aa7a95854be0c5423a925b7aee417
Clare <clareivers@gmail.com>
2024-04-02 23:05:13 +1100

    modularise course component

--- courseinfo/src/components/Course.jsx
+++ courseinfo/src/components/Course.jsx
@@ -1,9 +1,13 @@
+const Header = ({heading}) => <h2>{heading}</h2>
+
+const Part = ({name,exercises}) => <p>{name} {exercises}</p>
+
 const Course = ({course}) => {
     const total = course.parts.reduce((s,p) => s + p.exercises,0)
     return (
       <>
-        <h2>{course.name}</h2>
-        { course.parts.map( c => <p key={c.id}> {c.name} {c.exercises} </p>)}
+        <Header heading={course.name} /> 
+        { course.parts.map( c => <Part key={c.id} name={c.name} exercises={c.exercises} />)}
         <p><b>total of {total} exercises</b></p>
       </>
     )
